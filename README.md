fa740790724c178cbd36f281db1df4f01263bed1
chuxiaodong <2549369356@qq.com>
2024-01-24 23:28:12 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # my_clock
 一个提醒休息的计时器
+
+### 安装需求
+
+```bash
+# 推荐python版本3.7+
+# 推荐在conda或venv等虚拟环境中运行
+pip install -r requirements.txt
+```
+
+### 测试运行
+
+- 运行aclock.py
+
+### 打包
+
+```bash
+# 推荐在conda或venv等虚拟环境中运行，否则可能导致打包过大。
+# 推荐使用字母a开头的名称，若遇到问题，在任务管理器中可以轻松找到，并强制停止运行。
+pyinstaller --name=aclock aclock.py -w -F # -i aclock.ico # （可选）定制图标
+```
+
+### 设置开机启动
+
+```
+# 为打包好的exe文件创建快捷方式
+# 将快捷方式复制入“启动”文件夹
+# 在windows系统中，路径大致为：
+%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
+```
+
---
+++ aclock.py
@@ -0,0 +1,75 @@
+# -*- encoding: utf-8 -*-
+"""
+@Modify Time          @Author      @Version    @Description
+--------------      -----------    --------    ------------
+2023/3/13 14:43     chuxiaodong      1.0           None
+"""
+
+from os import listdir, path
+from sys import exit
+from time import sleep
+from random import choice
+from traceback import print_exc
+from win32con import MB_OK, MB_YESNO, IDNO, IDCLOSE
+from win32api import MessageBox
+from pygame import mixer
+
+
+time_work = 30
+time_break = 10
+loop_play_max = 10
+play = True
+music_dir = path.dirname(__file__)+"/music/"
+music_start_path = music_dir + "start/"
+music_end_path = music_dir + "end/"
+processes = []
+mixer.init()
+
+
+def show(show_s, start=True):
+    while len(processes) > 0:
+        processes.pop().terminate()
+    if play:
+        if start:
+            file_list = listdir(music_start_path)
+            file_list = music_start_path + choice(file_list)
+        else:
+            file_list = listdir(music_end_path)
+            file_list = music_end_path + choice(file_list)
+        mixer.music.load(file_list)
+        mixer.music.play(loop_play_max)
+        MessageBox(0, show_s, "提醒", MB_OK)
+        mixer.music.stop()
+    else:
+        MessageBox(0, show_s, "提醒", MB_OK)
+
+
+def choose_play():
+    global play
+    s = "是否播放音乐？\n将开始{}+{}循环计时。".format(time_work, time_break)
+    i = MessageBox(0, s, "提醒", MB_YESNO)
+    if i == IDNO:
+        play = False
+    elif i == IDCLOSE:
+        exit(0)
+
+
+if __name__ == '__main__':
+    try:
+        choose_play()
+        loop_time = 0
+        while True:
+            sleep(time_work * 60)
+            text = "休息{}分钟。".format(time_break)
+            show(text, False)
+            sleep(time_break * 60)
+            loop_time += 1
+            text = "休息结束，开始{}分钟工作。\n这是{}次循环。".format(time_work, loop_time)
+            show(text, True)
+    except:
+        f = open(music_dir + "error.txt", "w")
+        print_exc(file=f)
+        f.close()
+        MessageBox(0, "aclock程序出错，\n请查看error.txt。", "提醒", MB_OK)
+    finally:
+        exit(0)
