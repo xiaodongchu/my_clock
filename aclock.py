7923c700113333296b92cf0782c553e5ea5eadb5
chuxiaodong <2549369356@qq.com>
2024-01-26 09:40:35 +0800

    修复打包路径错误，优化休息逻辑。

--- aclock.py
+++ aclock.py
@@ -5,29 +5,33 @@
 2023/3/13 14:43     chuxiaodong      1.0           None
 """
 
-from os import listdir, path
-from sys import exit
+from os import listdir
 from time import sleep
 from random import choice
 from traceback import print_exc
-from win32con import MB_OK, MB_YESNO, IDNO, IDCLOSE
+from multiprocessing import Process as process
+
+from win32con import MB_OK, MB_YESNOCANCEL, IDYES, IDNO
 from win32api import MessageBox
 from pygame import mixer
 
-music_dir = "D:/programming/python/my_clock/music"  # 绝对路径
-time_work = 30
-time_break = 10
-loop_play_max = 10
+music_dir = "D:/programming/python/my_clock/music/"  # 绝对路径
+time_work = 30  # 单位：分钟
+time_break = 10  # 单位：分钟
+loop_play_max = 3
 play = True
 music_start_path = music_dir + "start/"
 music_end_path = music_dir + "end/"
-processes = []
+error_path = music_dir + "error.txt"
+process_list = []
 mixer.init()
 
 
 def show(show_s, start=True):
-    while len(processes) > 0:
-        processes.pop().terminate()
+    while len(process_list) > 0 and start:
+        process1 = process_list.pop()
+        if process1 is not None and process1.is_alive():
+            process1.terminate()
     if play:
         if start:
             file_list = listdir(music_start_path)
@@ -45,30 +49,36 @@ def show(show_s, start=True):
 
 def choose_play():
     global play
-    s = "是否播放音乐？\n将开始{}+{}循环计时。".format(time_work, time_break)
-    i = MessageBox(0, s, "提醒", MB_YESNO)
+    s = "是否播放音乐？\n将开始{}+{}循环计时。\n单击取消推出计时".format(time_work, time_break)
+    i = MessageBox(0, s, "提醒", MB_YESNOCANCEL)
     if i == IDNO:
         play = False
-    elif i == IDCLOSE:
-        exit(0)
+    elif i == IDYES:
+        play = True
+    else:
+        return 0
+    loop_run()
 
 
-if __name__ == '__main__':
+def loop_run():
     try:
-        choose_play()
         loop_time = 0
         while True:
             sleep(time_work * 60)
             text = "休息{}分钟。".format(time_break)
-            show(text, False)
+            process1 = process(target=show, args=(text, False))
+            process1.start()
+            process_list.append(process1)
             sleep(time_break * 60)
             loop_time += 1
             text = "休息结束，开始{}分钟工作。\n这是{}次循环。".format(time_work, loop_time)
             show(text, True)
     except:
-        f = open(music_dir + "error.txt", "w")
+        f = open(error_path, "w", encoding="utf-8")
         print_exc(file=f)
         f.close()
         MessageBox(0, "aclock程序出错，\n请查看error.txt。", "提醒", MB_OK)
-    finally:
-        exit(0)
+
+
+if __name__ == '__main__':
+    choose_play()
